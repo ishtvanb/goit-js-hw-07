ffb6d4c3b0cd52a9f45af8387a002e2a132b4c6e
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-09 19:34:30 +0300

    Додані зміни до сьомого ДЗ.

---
+++ js/01-gallery.js
@@ -0,0 +1,70 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const items = [];
+
+galleryItems.map((element) => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+
+    galleryLink.href = element.original;
+
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery__image');
+
+    galleryImg.src = element.preview;
+
+    galleryImg.setAttribute('data-source', element.original);
+
+    galleryImg.alt = element.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImg);
+    items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+document.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const imgSelected = evt.target.getAttribute('data-source');
+
+    const card = basicLightBox.create(
+        `<img src='${imgSelected}' width="800" height="600">`,
+        {
+            onShow: () => {
+                document.addEventListener('keydown', openModal);
+            },
+            onClose: () => {
+                document.removeEventListener('keydown', closeModal);
+            },
+        }
+    );
+
+    card.show();
+
+    function closeModal(evt) {
+        if (evt.key === 'Escape') {
+            card.close();
+        }
+    }
+});
+
+ 
+ 
+
+
+
+
+
+
+
